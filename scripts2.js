e62b1e92169c317252b56daff4d063fa5049d5b9
goldenmamba12 <ab17qk@brocku.ca>
2021-11-08 18:52:07 -0500

    Create scripts2.js

---
+++ scripts2.js
@@ -0,0 +1,15 @@
+function mix(){
+document.getElementById("blue").style.color="blue";
+document.getElementById("green").style.color="green";
+document.getElementById("red").style.color="red";
+
+
+	var first = "#AB00FF";
+	var second ="#00BB00";
+	var third = "#bb9320";
+
+	document.getElementById("blue").style.color=first;
+	document.getElementById("green").style.colour=second;
+	document.getElementById("red").style.color=third;
+
+}
