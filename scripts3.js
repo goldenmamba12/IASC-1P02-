7e8574a6c19cc569ef212aeb3bd4e6f7cf344f89
goldenmamba12 <ab17qk@brocku.ca>
2021-11-08 18:52:09 -0500

    Create scripts3.js

---
+++ scripts3.js
@@ -0,0 +1,8 @@
+
+function therest(){
+	document.write("Output Zero Here");
+	document.getElementById("output1").innerHTML="Output One Here";
+	document.getElementById("output2").innerHTML="Output Two Here";
+	document.getElementById("output3").innerHTML="Output Three Here";
+
+}
